2454aac4e58a5654d5bb092e3014c5dcee31b42f
ddduuufff <ericduford@gmail.com>
2020-11-05 11:46:09 -0500

    fixed my mistakes

--- run_analysis.R
+++ run_analysis.R
@@ -14,134 +14,147 @@ download.file(fileUrl, destfile = "./data/cleaning_dataset.zip", method = "curl"
 #    5. From the data set in step 4, creates a second, independent tidy data set with the average 
 #    	of each variable for each activity and each subject.
 
-#Step 1: download and unzip the data
-# Unzip dataSet to /data directory
-unzip(zipfile="./data/cleaning_dataset.zip", exdir="./data")
+# load required libraries
+library(data.table)
+library(dplyr)
+library(reshape2)
 
-#define the path where the new folder has been unziped
-pathdata = file.path("./data/UCI HAR Dataset")
-#create a file which has the 28 file names
-files = list.files(pathdata, recursive=TRUE)
-#show the files
-files
+# set the data set directory (default: "UCI HAR Dataset", trailing slash required)
+dataset.dir <- "./UCI HAR Dataset/"
 
-#Step 2: extract the test and training sets to create one data set
+#' Load subset of the full 'UCI HAR Dataset' function.
+#' 
+#' This function loads a subset of the full data set. The steps are:
+#' - load features table selecting only the std/mean columns 
+#' - load activities, assign descriptive labels (convert into a factor variable)
+#' - load subjects
+#' - filter the data table to select only mean and standard deviation columns
+#' - load labels and levels to the activity column and convert it into a factor variable
+#' - assign cleaned feature strings as column names for the feature measurements
+#' - create feature table appending the activities and subjects columns
+#'
+#' @param dataset.subset name of the data subset (i.e. 'train' or 'test')
+#'
+#' @return clean data table object
+load.data.subset <- function(dataset.subset) {
+        
+        if(!dir.exists(dataset.dir)) stop(sprintf("Data set directory not found: %s", dataset.dir))
+        
+        print(sprintf("loading data subset '%s'", dataset.subset))
+        
+        # helper function to load data table
+        load.data.table <- function(file.name, column.names) 
+                data.table::fread(paste0(dataset.dir, file.name), col.names = column.names)
+        
+        # helper function to create the data file path for the train/test subsets
+        create.datafile.path <- function(dataset.item) sprintf("%1$s/%2$s_%1$s.txt", dataset.subset, dataset.item)
+        
+        # helper function to create clean feature column names (replace '-' by '.' and remove '()')
+        create.clean.colname <- function(file.name) file.name <- gsub("-", ".", gsub("[()]", "", file.name))
+        
+        # load sdt/mean features from features file
+        features.column.names <- c("featurenumber", "featurename")
+        stdmean.features <- load.data.table("features.txt", features.column.names) %>% 
+                filter(grepl("mean|std", featurename))
+        
+        # load activity levels and labels
+        activity.labels <- load.data.table("activity_labels.txt", c("activity", "label"))
+        
+        # load activities and convert the vector into a factor variable using the activity table
+        # levels: WALKING WALKING_UPSTAIRS WALKING_DOWNSTAIRS SITTING STANDING LAYING
+        data.file.path.y <- create.datafile.path("y")
+        activities <- (load.data.table(data.file.path.y, c("activity"))$activity)  %>%
+                factor(levels=activity.labels$activity, labels=activity.labels$label)
+        
+        # load subjects and convert the vector to a factor variable with 
+        # levels: 1 2 3 4 5 6
+        data.file.path.subject <- create.datafile.path("subject")
+        subjects <- load.data.table(data.file.path.subject, c("subject"))$subject %>% factor(levels = c(1:30))
 
-        #test data set
-        #train data set
-        #features data set
-        #activity labels data set
+        # create feature table including the following actions:
+        # - load features table 
+        # - select std/mean features
+        # - assign cleaned column names (of selected features) 
+        # - bind activity column
+        # - move the last (binded) column to be the first one
+        data.file.path.X <- create.datafile.path("X")
+        result.table <- data.table::fread(paste0(dataset.dir,data.file.path.X)) %>%
+                select(stdmean.features[["featurenumber"]]) %>%
+                `colnames<-`( create.clean.colname(stdmean.features[["featurename"]]) ) %>%
+                mutate(activity=activities, subject=subjects) %>%
+                select(80:81, 1:79)
+}
 
+#' Function to merge the two data tables created for the 'train' and 'test' subsets.
+#' 
+#' @return In case of success the function returns the merged data table object.
+create.clean.dataset <- function() {
 
+        # load subset 'train'
+        data.subset.train <- load.data.subset("train")
 
-##  [1] "activity_labels.txt"                         
-##  [2] "features.txt"                                
-##  [3] "features_info.txt"                           
-##  [4] "README.txt"                                  
-##  [5] "test/Inertial Signals/body_acc_x_test.txt"   
-##  [6] "test/Inertial Signals/body_acc_y_test.txt"   
-##  [7] "test/Inertial Signals/body_acc_z_test.txt"   
-##  [8] "test/Inertial Signals/body_gyro_x_test.txt"  
-##  [9] "test/Inertial Signals/body_gyro_y_test.txt"  
-## [10] "test/Inertial Signals/body_gyro_z_test.txt"  
-## [11] "test/Inertial Signals/total_acc_x_test.txt"  
-## [12] "test/Inertial Signals/total_acc_y_test.txt"  
-## [13] "test/Inertial Signals/total_acc_z_test.txt"  
-## [14] "test/subject_test.txt"                       
-## [15] "test/X_test.txt"                             
-## [16] "test/y_test.txt"                             
-## [17] "train/Inertial Signals/body_acc_x_train.txt" 
-## [18] "train/Inertial Signals/body_acc_y_train.txt" 
-## [19] "train/Inertial Signals/body_acc_z_train.txt" 
-## [20] "train/Inertial Signals/body_gyro_x_train.txt"
-## [21] "train/Inertial Signals/body_gyro_y_train.txt"
-## [22] "train/Inertial Signals/body_gyro_z_train.txt"
-## [23] "train/Inertial Signals/total_acc_x_train.txt"
-## [24] "train/Inertial Signals/total_acc_y_train.txt"
-## [25] "train/Inertial Signals/total_acc_z_train.txt"
-## [26] "train/subject_train.txt"                     
-## [27] "train/X_train.txt"                           
-## [28] "train/y_train.txt"
+        # load subset 'test'
+        data.subset.test <- load.data.subset("test")
 
-At this stage the file are now unipped and there entire data set is ready to be evaluated. There are 28 files and that need to be analyzed. Refer above.
+        # merge data sets if they exist
+        if(exists("data.subset.train") & exists("data.subset.test")) {
+                print(sprintf("Merging 'train' data table (%d rows, %d columns) and 'test' data table (%d rows, %d columns)", 
+                        nrow(data.subset.train), ncol(data.subset.train), nrow(data.subset.test), ncol(data.subset.test)))
+                combined <- union(data.subset.test, data.subset.train)
+                
+        } else
+                stop("Error: data set cannot be created.")
+}
 
-Note - This is the basis for the entire project. Ensure that this milestone is reached.
-Data Analysis
+#' Function to create the data set file based on the clean data set table.
+#'
+#' @param clean.dataset clean data set
+#' @param clean.dataset.file.name clean data set file name
+create.clean.dataset.file <- function(clean.dataset, clean.dataset.file.name) {
+        print(sprintf("Creating file for merged data table (%d rows, %d columns)", nrow(clean.dataset), ncol(clean.dataset)))
+        if(exists("clean.dataset") ) 
+                write.table(clean.dataset, clean.dataset.file.name, row.names = FALSE, quote = FALSE)
+}
 
-In the data the readme.me document, you will get a detailed perspective of what to expect and how to manipulate the data. There are three core variables:
+#' Function to create the tidy data set based on the clean data set.
+#' 
+#' @return In case of success the function returns the merged data table object.
+create.tidy.dataset <- function(clean.dataset) {
+        
+        # id vars for melting the data set
+        melt.id.vars = c("subject", "activity")
+        
+        # measurement column names (excluding the id vars)
+        melt.measure.vars = colnames(clean.dataset)[!colnames(clean.dataset) %in% c("subject", "activity")]
+        
+        # meldted data
+        melted.data = melt(clean.dataset, id = melt.id.vars, measure.vars = melt.measure.vars)
+        
+        # create data set for average of each measurement variable for each activity and subject
+        dcast(melted.data, subject + activity ~ variable, mean)
+}
 
-        Main
-        2. Test 3. Train
+#' Function to create the tidy data set file based on the clean data set table.
+#'
+#' @param clean.dataset clean data set
+#' @param tidy.dataset.file.name tidy data set file name
+create.tidy.dataset.file <- function(tidy.dataset, tidy.dataset.file.name) {
+        tidy.dataset.file.name <- tidy.dataset.file.name
+        write.table(tidy.dataset, tidy.dataset.file.name, row.names = FALSE, quote = FALSE)
+}
 
-Main : activity_labels Inertial Signals Inertial Signals Test: features subject_test subject_train Train: features.info X_test X_train
+# execute functions
+clean.dataset <- create.clean.dataset()
+clean.dataset.file.name <- "clean_dataset.txt"
+create.clean.dataset.file(clean.dataset, clean.dataset.file.name)
 
-README y_test y_train ‘features_info.txt’: Shows information about the variables used on the feature vector. * - ‘features.txt’: List of all features. * - ‘activity_labels.txt’: Links the class labels with their activity name. * - ‘train/X_train.txt’: Training set. * - ‘train/y_train.txt’: Training labels. * - ‘test/X_test.txt’: Test set. *- ‘test/y_test.txt’: Test labels
-
-Analysis shows that you can categorize the data into 4 segments * training set * test set * features * activity labels
-
-Inertial Signal data is not required. Additionally, features and activity label are more for tagging and descriptive than data sets.
-Making the Test and Training Set Data
-
-The objective here is to make the test and training data as per the sequence stated above. 4 basic level data sets will be defined and created:
-
-        test data set
-        train data set
-        features data set
-        activity labels data set
-
-### 1.Making the test and training sets
-xtrain = read.table(file.path(pathdata, "train", "X_train.txt"),header = FALSE)
-ytrain = read.table(file.path(pathdata, "train", "y_train.txt"),header = FALSE)
-subject_train = read.table(file.path(pathdata, "train", "subject_train.txt"),header = FALSE)
-#Reading the testing tables
-xtest = read.table(file.path(pathdata, "test", "X_test.txt"),header = FALSE)
-ytest = read.table(file.path(pathdata, "test", "y_test.txt"),header = FALSE)
-subject_test = read.table(file.path(pathdata, "test", "subject_test.txt"),header = FALSE)
-#Read the features data
-features = read.table(file.path(pathdata, "features.txt"),header = FALSE)
-#Read activity labels data
-activityLabels = read.table(file.path(pathdata, "activity_labels.txt"),header = FALSE)
-
-### 2. Uses descriptive activity names to name the activities in the data set
-#Create Sanity and Column Values to the Train Data
-colnames(xtrain) = features[,2]
-colnames(ytrain) = "activityId"
-colnames(subject_train) = "subjectId"
-#Create Sanity and column values to the test data
-colnames(xtest) = features[,2]
-colnames(ytest) = "activityId"
-colnames(subject_test) = "subjectId"
-#Create sanity check for the activity labels value
-colnames(activityLabels) <- c('activityId','activityType')
-
-
-###3. Merge the data sets
-#Merging the train and test data - important outcome of the project
-mrg_train = cbind(ytrain, subject_train, xtrain)
-mrg_test = cbind(ytest, subject_test, xtest)
-#Create the main data table merging both table tables - this is the outcome of 1
-setAllInOne = rbind(mrg_train, mrg_test)
-
-
-### 4. extracting mean and SD
-
-# Need step is to read all the values that are available
-colNames = colnames(setAllInOne)
-#Need to get a subset of all the mean and standards and the correspondongin activityID and subjectID 
-mean_and_std = (grepl("activityId" , colNames) | grepl("subjectId" , colNames) | grepl("mean.." , colNames) | grepl("std.." , colNames))
-#A subtset has to be created to get the required dataset
-setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
-
-### 5. Name data set
-setWithActivityNames = merge(setForMeanAndStd, activityLabels, by='activityId', all.x=TRUE)
-
-
-### 6. Name the data set
-# New tidy set has to be created 
-secTidySet <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
-secTidySet <- secTidySet[order(secTidySet$subjectId, secTidySet$activityId),]
-
-
-### 7. Save the data
-#The last step is to write the ouput to a text file 
-write.table(secTidySet, "secTidySet.txt", row.name=FALSE)
+# If the clean data set exists, create tidy data set 
+if(exists("clean.dataset")) {
+        tidy.dataset <- create.tidy.dataset(clean.dataset)
+        print(sprintf("Clean data set file created: %s ", clean.dataset.file.name))
+        # create tidy data set file
+        tidy.dataset.file.name <- "tidy_dataset.txt"
+        create.tidy.dataset.file(tidy.dataset, tidy.dataset.file.name)
+        if(file.exists(tidy.dataset.file.name)) 
+                print(sprintf("Tidy data set file created: %s ", tidy.dataset.file.name))
+}
